850835f3dd5a2cb74b74b89bca51df8fdf194abe
Jiwon Seok <62790140+myomu@users.noreply.github.com>
2024-05-19 12:31:23 +0900

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  const target = process.env.REACT_APP_PROXY_TARGET || 'http://localhost:8080'; // 기본값을 설정하거나 환경변수를 사용
+  const target = "https://port-0-joshuaam-1ru12mlwbog2dc.sel5.cloudtype.app" || 'http://localhost:8080'; // 기본값을 설정하거나 환경변수를 사용
 
   app.use(
     '/api',
